5d69f926a1076a1dfc6b47f49c0d12e9f65afae4
code33dev <alberto.huerta.luna@gmail.com>
2024-11-28 18:27:29 -0600

    Update build-sdk.sh

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,11 +1,13 @@
-sh linux-scripts/update-repos.sh
-# sh linux-scripts/install-frameworks.sh 9
-# sh linux-scripts/install-frameworks.sh 10
-# sh linux-scripts/install-frameworks.sh 11
-# sh linux-scripts/install-frameworks.sh 12
-# sh linux-scripts/install-frameworks.sh 13
-sh linux-scripts/install-frameworks.sh 14
-# sh linux-scripts/install-frameworks.sh 15
+rm -rf sdk 
+rm -rf sdk_old
+sh scripts/update-repos.sh
+sh scripts/install-frameworks.sh 9
+sh scripts/install-frameworks.sh 10
+sh scripts/install-frameworks.sh 11
+sh scripts/install-frameworks.sh 12
+sh scripts/install-frameworks.sh 13
+sh scripts/install-frameworks.sh 14
+sh scripts/install-frameworks.sh 15
 #mv sdk*.xz sandbox
 #cp -r --remove-destination sdk-* sandbox-sdk-folders/
 #rm -rf sdk-gcc*
