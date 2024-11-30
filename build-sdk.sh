ff6adc9815971c5adc2847243356786279297460
code33dev <alberto.huerta.luna@gmail.com>
2024-11-30 17:27:16 -0600

    Update build-sdk.sh

--- build-sdk.sh
+++ build-sdk.sh
@@ -1,5 +1,8 @@
+timestamp1=$(date +%Y-%m-%d_%H-%M-%S)
+
 rm -rf sdk 
 rm -rf sdk_old
+rm -rf frameworks
 sh scripts/update-repos.sh
 sh scripts/install-frameworks.sh 9
 sh scripts/install-frameworks.sh 10
@@ -8,6 +11,33 @@ sh scripts/install-frameworks.sh 12
 sh scripts/install-frameworks.sh 13
 sh scripts/install-frameworks.sh 14
 sh scripts/install-frameworks.sh 15
+
+timestamp2=$(date +%Y-%m-%d_%H-%M-%S)
+
+start_time=$(date -d "$timestamp1" +%s)
+end_time=$(date -d "$timestamp2" +%s)
+total_time=$((end_time - start_time))
+
+echo "Started at: $timestamp1"
+echo "Finished at: $timestamp2"
+echo "Total time: $total_time seconds"
+
+
+# timestamp1=$(date +%Y-%m-%d_%H-%M-%S)
+# rm -rf sdk 
+# rm -rf sdk_old
+# rm -rf frameworks
+# sh scripts/update-repos.sh
+# sh scripts/install-frameworks.sh 9
+# sh scripts/install-frameworks.sh 10
+# sh scripts/install-frameworks.sh 11
+# sh scripts/install-frameworks.sh 12
+# sh scripts/install-frameworks.sh 13
+# sh scripts/install-frameworks.sh 14
+# sh scripts/install-frameworks.sh 15
+# timestamp2=$(date +%Y-%m-%d_%H-%M-%S)
+
+ 
 #mv sdk*.xz sandbox
 #cp -r --remove-destination sdk-* sandbox-sdk-folders/
 #rm -rf sdk-gcc*
