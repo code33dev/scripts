9815e7d5a34df977230a6adddbd141286b86975b
code33dev <alberto.huerta.luna@gmail.com>
2025-01-17 12:15:09 -0600

    Update imgui.sh

--- imgui.sh
+++ imgui.sh
@@ -1,5 +1,9 @@
 cd frameworks
 cd imgui 
+sed -i 's|#include <SDL.h>|#include <SDL2/SDL.h>|' backends/imgui_impl_sdl2.cpp
+sed -i 's|#include <SDL_syswm.h>|#include <SDL2/SDL_syswm.h>|' backends/imgui_impl_sdl2.cpp
+sed -i 's|#include <SDL_vulkan.h>|#include <SDL2/SDL_vulkan.h>|' backends/imgui_impl_sdl2.cpp
+
 #cp imgui_demo.cpp /mnt/development/imgui/framework/ &&
 cmake_content=$(cat <<'EOF'
 cmake_minimum_required(VERSION 3.0)
@@ -16,7 +20,7 @@ set(IMGUI_SRC
 
 # Source files for OpenGL2, OpenGL3, and GLFW backends
 set(IMGUI_BACKEND_SRC
-    backends/imgui_impl_glfw.cpp
+    backends/imgui_impl_sdl2.cpp
     backends/imgui_impl_opengl3.cpp
     backends/imgui_impl_opengl2.cpp
 )
@@ -32,9 +36,9 @@ target_include_directories(imgui PUBLIC
 
 # Link necessary libraries (GLFW, OpenGL)
 find_package(OpenGL REQUIRED)
-find_package(glfw3 REQUIRED)
+find_package(SDL2 REQUIRED)
 
-target_link_libraries(imgui PUBLIC OpenGL::GL glfw)
+target_link_libraries(imgui PUBLIC OpenGL::GL SDL2)
 
 # Installation configuration for the static library
 install(TARGETS imgui
@@ -49,7 +53,7 @@ install(FILES
     imstb_rectpack.h
     imstb_textedit.h
     imstb_truetype.h
-    backends/imgui_impl_glfw.h
+    backends/imgui_impl_sdl2.h
     backends/imgui_impl_opengl3.h
     backends/imgui_impl_opengl2.h
     DESTINATION include/imgui
@@ -64,3 +68,40 @@ make
 make install 
 cd .. 
 cd ..
+
+#!/bin/bash
+
+# Create the target directories
+# Brotli
+cd brotli
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../sdk .. 
+make -j$(nproc)
+make install
+cp -r ../c/include/brotli ../../sdk/include/
+cp libbrotlidec.a libbrotlienc.a libbrotlicommon.a ../../sdk/lib/
+cd ../../
+
+
+# HarfBuzz
+cd harfbuzz
+mkdir build 
+cd build
+cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=../../sdk .. 
+make -16
+make install
+cd ..
+cd ..
+
+
+
+# BZip2
+cd bzip2
+mkdir build
+cd build
+cmake -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF -DCMAKE_INSTALL_PREFIX=../../sdk .. 
+make 
+make install
+cd ..
+cd ..
+
